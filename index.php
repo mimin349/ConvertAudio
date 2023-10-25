26bcba49be9667dc5f312302fc53a2f07837131e
mimin349 <sidshadow09@gmail.com>
2023-10-25 17:19:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Selamat Datang!</h1>
+    </header>
+    <div>
+        <p>Terima kasih.</p>
+    </div>
+    <?php
+        header("Location: index.php");
+        exit;
+    ?>
+</body>
+</html>
